e215dbbdaebe871b7214be76bb694aa2764187e6
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-10-01 23:35:43 +0700

    - BaseGameObject

---
+++ Technical/Assets/Scripts/BaseGameObject/BaseGameObject.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseGameObject : MonoBehaviour {
+
+    public int id;
+    public int status = -1;//trang thai cua Enemy: -1 die, 0 move, 1 attack
+    public float hp = 300;
+    public float damge = 0;
+
+    //di chuyen
+    public float speed;
+    public float timeDelayAttack;
+
+    public bool pause = false;
+
+    public Animator animator;
+    public Health health;
+    public Transform posNumberHit;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public virtual void Move()
+    {
+
+    }
+    public virtual void Attack()
+    {     
+    }
+    public virtual void Die()
+    {
+    }
+}
--- Technical/Assets/Scripts/Enemy/Enemy.cs
+++ Technical/Assets/Scripts/Enemy/Enemy.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 
 public class Enemy : BaseGameObject
-{
-
-    
+{    
 
 	// Use this for initialization
 	void Start () {
