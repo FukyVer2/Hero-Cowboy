ec7711664331e3844c4d72dfb9bb60ae8688702f
toneylee <ducsangpro.93@gmail.com>
2015-10-12 17:06:07 +0700

    Lam tower

--- Technical/Assets/Scripts/Bullet/TowerBullet.cs
+++ Technical/Assets/Scripts/Bullet/TowerBullet.cs
@@ -1,19 +1,132 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TowerBullet : MonoBehaviour
+public class TowerBullet : Bullet
 {
-    public float speed = 40;
-    public bool isLeftDirection = true;
-    public float damge = 100;
 
-    public void Move()
+    //public float speed = 40;
+    //public float damge = 100;
+    //private float posX;
+    //private float posY;
+    //private float vx;
+    //private float vy;
+    public float scaleUnit;
+    public Vector3 target;
+
+    void Start()
     {
-        float posX = gameObject.transform.position.x;
-        posX += speed * Time.deltaTime;
-        gameObject.transform.position = new Vector3(posX, 0, 0);
+        //posX = gameObject.transform.position.x;
+        //posY = gameObject.transform.position.y;
+        //vx = velocityX;
+        //vy = velocityY;
+        //target = new Vector3(20, -40, 0);
     }
 
+    public override void ResetProperties()
+    {
+        //posX = gameObject.transform.position.x;
+        //posY = gameObject.transform.position.y;
+        //vx = velocityX;
+        //vy = velocityY;
+    }
+
+    public void SetTarget(Vector3 _target)
+    {
+        this.target = _target;
+    }
+
+    public override void InitBullet(Vector3 _positionStart, BulletDirection _direction)
+    {
+        //Khởi tạo ban đầu của đạn gồm một số thông số:
+        // + Hướng di chuyển của đạn
+        // + Rotate hình
+        // + Thay đổi giá trị tốc độ di chuyển của đạn
+        gameObject.transform.localPosition = _positionStart;
+        direction = _direction;
+        //Rotate(90);
+        switch (direction)
+        {
+            case BulletDirection.LEFT:
+                {
+                    speedCurrent = -speed;
+                    FlipHorizontal(180);
+                    break;
+                }
+            case BulletDirection.RIGHT:
+                {
+                    speedCurrent = speed;
+                    FlipHorizontal(0);
+                    break;
+                }
+            case BulletDirection.NONE:
+                {
+                    speedCurrent = 0;
+                    FlipHorizontal(0);
+                    break;
+                }
+        }
+        // Rotate(-90);
+    }
+
+    public float CalAngleWithTarget()
+    {
+        Vector3 positionTarget = target;
+        Vector3 relative = positionTarget - transform.position;//transform.InverseTransformPoint(target.position);
+        float angle = Mathf.Atan2(relative.y, relative.x) * Mathf.Rad2Deg;
+
+        return angle;
+    }
+
+    public float CalDistanceWithTarget()
+    {
+        Vector3 positionTarget = target;
+        return Vector3.Distance(positionTarget, transform.position);
+    }
+
+    public void AppearBullet()
+    {
+        float angle = CalAngleWithTarget();
+        float distance = CalDistanceWithTarget();
+        transform.localScale = new Vector3(transform.localScale.x, distance * scaleUnit , transform.localScale.z);
+        iTween.ScaleTo(gameObject, iTween.Hash("isLocal",true,
+                                                "scale",new Vector3(0.4f, 0.5f, 0.5f), 
+                                                iT.ScaleTo.time, 0.7f,
+                                                //iT.ScaleTo.easetype, iTween.EaseType.easeInSine,
+                                                iT.ScaleTo.oncomplete, "Complete"));
+        Rotate(-270 + angle);
+    }
+
+    private void Complete()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    public override void Move()
+    {
+        ///*
+        //float posX = gameObject.transform.position.x;
+        //posX += speedCurrent * Time.deltaTime;
+        //gameObject.transform.position = new Vector3(posX, gameObject.transform.position.y, 0);
+        //*/
+        //vx += accelerationX * Time.deltaTime;
+        //vy += accelerationY * Time.deltaTime;
+
+        //if (direction == BulletDirection.LEFT)
+        //    posX -= vx * Time.deltaTime;
+        //else if (direction == BulletDirection.RIGHT)
+        //    posX += vx * Time.deltaTime;
+
+        //posY += vy * Time.deltaTime;
+
+        //gameObject.transform.position = new Vector3(posX, posY, 0);
+    }
+
+    public virtual void KillEnemies()
+    {
+        //Nếu là enemy thì tiêu diệt player
+        //Ngược lại thì tiêu diệt enemies
+    }
+    /*
     public void ChangeDirection(bool direction)
     {
         if (direction)
@@ -26,18 +139,53 @@ public class TowerBullet : MonoBehaviour
             speed = (speed > 0) ? speed : -speed;
             gameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
         }
+      
     }
 
-    public void Destroy()
+    public void ChangeDirection(BulletDirection direction)
     {
-        if (gameObject.transform.position.x >= Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x ||
-            gameObject.transform.position.x <= -Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, 0, 0)).x)
-            PoolObject.Instance.DespawnObject(gameObject.transform, "Bullet");
+        switch(direction){
+            case BulletDirection.LEFT:
+                {
+                    speed = (speed < 0) ? speed : -speed;
+                    gameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, 0));
+                    break;
+                }
+            case BulletDirection.RIGHT:
+                {
+                    speed = (speed > 0) ? speed : -speed;
+                    gameObject.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+                    break;
+                }
+            case BulletDirection.NONE:
+                {
+                    speed = 0;
+                    break;
+                }
+        }
+
+    }
+    */
+    public override void Die()
+    {
+        base.Die();
     }
 
-    public void Update()
+    public override void BulletUpdate()
     {
-        Move();
-        Destroy();
+        //Move();
+        //Die();
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //if(col.tag =="Enemy")
+        //{
+        //    ManagerObject.Instance.RenderParticalEnemy(ObjectType.ENEMY_HIT, transform.position);
+        //    Enemy enemy = col.GetComponent<Enemy>();
+        //    if (enemy != null)
+        //        enemy.Hit(damge);
+        //    PoolObject.Instance.DespawnObject(gameObject.transform, "Bullet");
+
+        //}
     }
 }
---
+++ Technical/Assets/Scripts/Player/Tower.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tower : MonoBehaviour {
+
+    public Transform shootPosition; //Vi tri vien dan se bay ra
+    private bool isShoot; //Duoc phep ban
+    public float timeShootWait; //Thoi gian dan cach giua cac luot ban
+    public Vector2 range; //Range cua tower
+    private BoxCollider2D boxCollider2D;
+    private List<GameObject> enemyInBoxs;
+    public GameObject towerBulletPrefab;
+
+    void Start()
+    {
+        boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
+        boxCollider2D.size = range;
+        enemyInBoxs = new List<GameObject>();
+        isShoot = true;
+    }
+
+    void Update()
+    {
+        TowerShoot();
+        //enemyInBoxs.RemoveAll(enemy => enemy == null);
+    }
+
+    private void AllowShoot()
+    {
+        this.isShoot = true;
+    }
+
+    private void TowerShoot()
+    {
+        GameObject enemyObj = this.GetEnemyObjectInBoxs();
+        if (enemyObj != null)
+        {
+            if (isShoot)
+            {
+                isShoot = false;
+                Invoke("AllowShoot", timeShootWait);
+                GameObject bulletObject = PoolObject.Instance.SpawnObject(towerBulletPrefab, "Bullet");
+                TowerBullet bullet = bulletObject.GetComponent<TowerBullet>();
+                bullet.InitBullet(shootPosition.position, BulletDirection.NONE);
+                bullet.SetTarget(enemyObj.transform.position);
+                bullet.AppearBullet();
+            }
+        }
+    }
+
+    public GameObject GetEnemyObjectInBoxs()
+    {
+        if (this.enemyInBoxs.Count > 0)
+        {
+            GameObject enemyObj = this.enemyInBoxs[0];
+            this.enemyInBoxs.Remove(enemyObj);
+            return enemyObj;
+        }
+        return null;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Enemy") { 
+            //Enemy enemy = col.GetComponent<Enemy>();
+            enemyInBoxs.Add(col.gameObject);
+        }
+    }
+}
