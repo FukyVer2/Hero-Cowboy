7f87028b0c9cf2957036541736aa10325c372cfd
Nguyễn Thanh Cao <caont.fuky@gmail.com>
2015-11-11 22:22:02 +0700

    - Fix bug @Yen

--- Technical/Assets/Scripts/Object/Enemy/EnemyFly/EnemyFly.cs
+++ Technical/Assets/Scripts/Object/Enemy/EnemyFly/EnemyFly.cs
@@ -1,6 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum Direction
+{
+    NONE = 0,
+    LEFT = 1,
+    RIGHT = 2
+}
 public class EnemyFly : Enemy {
 
     public float speedX;
@@ -9,12 +15,15 @@ public class EnemyFly : Enemy {
     private float x = 0;
     private float y = 0;
     private float posXPlayer = 0;
+    private float posStart;
+    public Direction dir; 
     public override void Init(int _level, float _speed, float _hp, float _damge)
     {
         base.Init(_level, _speed, _hp, _damge);
     }
 	// Use this for initialization
-	void Start () { 
+	void Start () {
+        posStart = transform.position.x;
     }
 	
 	// Update is called once per frame
@@ -40,21 +49,55 @@ public class EnemyFly : Enemy {
     public override void Move()
     {
         base.Move();
-        if (Mathf.Abs(transform.position.x - posXPlayer) > 2)
+
+        if(transform.position.x > posXPlayer)
         {
-            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+            dir = Direction.RIGHT;
         }
         else
         {
-            //attack
-            Invoke("PhoenixAttack", 2);
+            dir = Direction.LEFT;
         }
+        Idle();
+
+        //if (Mathf.Abs(transform.position.x - posXPlayer) > 2)
+        //{
+        //    transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+        //}
+        //else
+        //{
+        //    //attack
+        //    Invoke("PhoenixAttack", 2);
+        //}
     }
     void PhoenixAttack()
     {
-        Debug.Log("Begin Attack");
-
-        transform.position += new Vector3(speedX, Mathf.Sin(transform.position.x) * -5) *Time.deltaTime;
+        //speedX = Mathf.Abs(speedX);
+        //switch(dir)
+        //{
+        //    case Direction.NONE:
+        //        break;
+        //    case Direction.LEFT:
+        //        speedX = speedX;
+        //        break;
+        //    case Direction.RIGHT:
+        //        speedX = -speedX;
+        //        break;
+        //}
+        if (dir != Direction.NONE)
+        {
+            transform.position += new Vector3(speedX, Mathf.Sin(transform.position.x) * -5) * Time.deltaTime;
+        }
+    }
+    void Idle()
+    {
+        float posTarget = posStart - ((posStart - posXPlayer) * 2);
+        //if(Mathf.Abs(transform.position.x - posTarget) <= 0)
+        if(transform.position.x < -2.15f)
+        {
+            dir = Direction.NONE;
+        }
+        PhoenixAttack();
     }
     public override void SetSpeed(int isRight)
     {
